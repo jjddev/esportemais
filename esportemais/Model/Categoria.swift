d922cf54d28b549c76e1de284cdb5140d3451184
PUCPR <pucpr@LADMAC0120.local>
2019-04-22 19:09:03 -0300

    Models categoria e evento

---
+++ esportemais/Model/Categoria.swift
@@ -0,0 +1,19 @@
+//
+//  Categoria.swift
+//  esportemais
+//
+//  Created by PUCPR on 22/04/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import Foundation
+
+class Categoria {
+    var id: Int
+    var descricao: String
+    
+    init(){
+        
+    }
+    
+}
---
+++ esportemais/Model/Evento.swift
@@ -0,0 +1,20 @@
+//
+//  Evento.swift
+//  esportemais
+//
+//  Created by PUCPR on 22/04/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import Foundation
+
+class Evento {
+    var id: Int
+    var nome: String
+    var data: Date
+    
+    
+    init(){
+        
+    }
+}
