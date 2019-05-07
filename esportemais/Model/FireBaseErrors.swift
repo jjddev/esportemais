31b4564dc2ff7f433ffce0592fef66fb3ec9c3f8
juliano <julianojd@ymail.com>
2019-05-07 02:55:18 -0300

    Formatação

--- esportemais/Model/FireBaseErrors.swift
+++ esportemais/Model/FireBaseErrors.swift
@@ -10,11 +10,12 @@ import Foundation
 
 
 class FireBaseErrors{
-    static let codes = [0: "Erro desconhecido",
-                        17008: "Formato de email inválido",
-                        //17007: "O email já cadastrado",
-                        17009: "Usuário ou senha inválidos",
-                        17011: "Usuário ou senha inválidos",
-                        17026: "A senha deve ter no minimo 6 caractres"
-    ]
+    static let codes = [
+                            0: "Erro desconhecido",
+                            17008: "Formato de email inválido",
+                            17007: "O email já cadastrado",
+                            17009: "Usuário ou senha inválidos",
+                            17011: "Usuário ou senha inválidos",
+                            17026: "A senha deve ter no minimo 6 caractres"
+                        ]
 }
