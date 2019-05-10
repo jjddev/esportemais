7339b17903426852a7462219c985b5200b1d25b8
PUCPR <pucpr@LADMAC0120.local>
2019-05-10 20:38:21 -0300

    Locations

--- esportemais/Controller/LocalViewController.swift
+++ esportemais/Controller/LocalViewController.swift
@@ -33,7 +33,7 @@ class LocalViewController: UIViewController, CLLocationManagerDelegate {
         vMapa.mapType = .standard
         vMapa.isZoomEnabled = true
         vMapa.isScrollEnabled = true
-        
+
         if let coor = vMapa.userLocation.location?.coordinate{
             vMapa.setCenter(coor, animated: true)
         }
@@ -43,8 +43,12 @@ class LocalViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last{
-            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+            
+            let logitude = -49.251087
+            let latitude = -25.451739
+            
+            let center = CLLocationCoordinate2D(latitude: latitude, longitude: logitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
             self.vMapa.setRegion(region, animated: true)
         }
     }
